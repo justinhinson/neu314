756cbc7def85d774aeacc95d6acf9fe1dfe61378
Justin Hinson <hinsonjustin@gmail.com>
2019-10-04 14:02:57 -0400

    Created subplots

--- ex3.jl
+++ ex3.jl
@@ -1,5 +1,5 @@
 picture = imread("el-capitan.png")
-imshow(picture)
+# imshow(picture)
 function getRGB(filename)
     load_pic = imread(filename)
     imshow(load_pic)
@@ -8,4 +8,17 @@ function getRGB(filename)
     blue = load_pic[:, :, 3]
     return red, green, blue
 end
-getRGB("el-capitan.png")
+#getRGB("el-capitan.png")
+#println("Reset with git reset --mixed")
+pic_red, pic_green, pic_blue = getRGB("el-capitan.png")
+image2 = zeros((360,640,3))
+image2[:, :, 1] = pic_green
+image2[:, :, 2] = pic_blue
+image2[:, :, 3] = pic_red
+figure(figsize=[500,700])
+title("Image RGB Comparison")
+axis("off")
+subplot(1,2,1)
+imshow(picture)
+subplot(1,2,2)
+imshow(image2)
