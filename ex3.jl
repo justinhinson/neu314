6c126dda3aaa287dc13460ed00b0b9cff6b60a0d
Justin Hinson <hinsonjustin@gmail.com>
2019-10-04 17:35:13 -0400

    Circular Rotation

--- ex3.jl
+++ ex3.jl
@@ -8,8 +8,8 @@ function getRGB(filename)
     blue = load_pic[:, :, 3]
     return red, green, blue
 end
-#getRGB("el-capitan.png")
-#println("Reset with git reset --mixed")
+getRGB("el-capitan.png")
+println("Reset with git reset --mixed")
 pic_red, pic_green, pic_blue = getRGB("el-capitan.png")
 image2 = zeros((360,640,3))
 image2[:, :, 1] = pic_green
@@ -22,3 +22,28 @@ subplot(1,2,1)
 imshow(picture)
 subplot(1,2,2)
 imshow(image2)
+function circularRotateRed(image, p)
+    red_channel = image[:, :, 1]
+    num_rows = size(red_channel)[1]
+    first = red_channel[1,: ]
+    shuffler = 0
+    while shuffler < p
+        for i=1:num_rows - 1
+            if i == num_rows - 1
+                red_channel[i,: ] = first
+            else
+                red_channel[i,: ] = red_channel[i + 1,: ]
+            end
+        end
+        shuffler = shuffler + 1
+    end
+    return image
+end
+test_image = circularRotateRed(picture, 180)
+figure(figsize=[500,700])
+title("Image Rotation Comparison")
+axis("off")
+subplot(1,2,1)
+imshow(picture)
+subplot(1,2,2)
+imshow(test_image)
